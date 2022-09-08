d589c28ed4349735d4eaf3708f90503bb254abc9
AleksandrSmirnov92 <ryan00@mail.ru>
2022-09-08 10:23:46 +0300

    исправил ошибку

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -9,7 +9,7 @@ const Header = ({ counter }: Props) => {
   return (
     <header className={HeaderCSS.header}>
       <div className={HeaderCSS.header_wrapper}>
-        <span className={HeaderCSS.header_span}>Осталось выполнитьmm</span>
+        <span className={HeaderCSS.header_span}>Осталось выполнить</span>
 
         <span className={HeaderCSS.header_span2}>{counter}</span>
       </div>

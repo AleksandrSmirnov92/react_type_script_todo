7e937830e760abec2e425a08f2d210515e0f3479
AleksandrSmirnov92 <ryan00@mail.ru>
2022-09-09 10:41:01 +0300

    Исправил замечания в папке filter

--- src/components/filter/filter.tsx
+++ src/components/filter/filter.tsx
@@ -1,17 +1,18 @@
 import React, { useRef } from 'react';
 import FilterCSS from './filter.module.css';
 import { FilterValue } from '../typemodule/types';
-interface FilterProps {
-  changeFilter: (value: string) => void;
-}
-// можно просто 
+
+// можно просто
 //const Filter: React.FC<TypeForFC> = ({ changeFilter }) => {
-const Filter = ({ changeFilter }: FilterProps) => {
+const Filter: React.FC<{ changeFilter: (value: string) => void }> = ({
+  changeFilter,
+}) => {
   // value
-  let valuee = useRef<HTMLSelectElement>(null);
+  let value = useRef<HTMLSelectElement>(null);
+
   let change = () => {
-    if (valuee && valuee.current) {
-      changeFilter(valuee.current.value);
+    if (value && value.current) {
+      changeFilter(value.current.value);
     }
   };
 
@@ -19,15 +20,15 @@ const Filter = ({ changeFilter }: FilterProps) => {
    * Сейчас такой warning есть, попробуй понять почему оно так и как это поправить 😀
    * react-dom.development.js:86 Warning: Use the `defaultValue` or `value` props on <select> instead of setting `selected` on <option>.
    */
-
+  //Исправил потому что реакт использует defoultValue вместо selected и дефотное значение должно совпадать со значением выбранной опцией!
   return (
     <div className={FilterCSS.filter_block}>
       <select
-        ref={valuee}
+        ref={value}
         onChange={change}
         className={FilterCSS.filter}
-        defaultValue={FilterValue.DEFAULT}>
-        <option id="1" value={FilterValue.ALL} selected>
+        defaultValue={FilterValue.ALL}>
+        <option id="1" value={FilterValue.ALL}>
           Выбрать все{' '}
         </option>
         <option id="2" value={FilterValue.ALLACTIVE}>
